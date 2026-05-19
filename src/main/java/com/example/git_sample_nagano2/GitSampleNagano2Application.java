93ff06019464060049a1a4f5d58e8344d6a5c3ca
NaganoRyotaNV <nagano30123012@gmail.com>
2026-05-19 13:42:39 +0900

    first commit

---
+++ src/main/java/com/example/git_sample_nagano2/GitSampleNagano2Application.java
@@ -0,0 +1,13 @@
+package com.example.git_sample_nagano2;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GitSampleNagano2Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GitSampleNagano2Application.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/git_sample_nagano2/GitSampleNagano2ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.git_sample_nagano2;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GitSampleNagano2ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
